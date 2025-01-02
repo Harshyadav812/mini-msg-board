3312d20198f86cb3d912a485f3d1f2c9c0498f33
Harshyadav812 <raoharsh138@gmail.com>
2025-01-02 11:43:23 +0000

    Add a fallback port to the app

--- app.js
+++ app.js
@@ -5,12 +5,17 @@ const { indexRouter } = require('./routes/indexRouter');
 const formRouter = require('./routes/formRouter');
 const msgRouter = require('./routes/msgRouter');
 
+const PORT = process.env.PORT || 3000;
+
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
 
-app.listen(3000)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 app.use('/new', formRouter);
 app.use('/message', msgRouter);
